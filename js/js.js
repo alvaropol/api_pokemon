280154351eb96fcfd0452816eee35dee2830d649
Manolo <mmgmolina03@gmail.com>
2023-10-03 23:54:08 +0200

    terminado la maquetacion de los nombres de los items y haciendo el modal de los detalles de los items

--- js/js.js
+++ js/js.js
@@ -6,8 +6,21 @@ $(document).ready(function(){
     }).done(function (resp) {
         var listaItems = resp.results;
         listaItems.forEach(item => {
-            var template = `<p> ${item.name} <p>`;
+            var template = `<div> ${item.name} <div>`;
             $('#lista-nomItems').append(template);
         });
     });
+    $(document).on('click', '.item', function () {
+        var itemId = $(this).attr('itemid');
+
+        $.ajax({
+            url: 'https://pokeapi.co/api/v2/item/' + itemId,
+            type: 'GET' 
+
+        }).done(function (resp) {
+            $('#modal-item').html(resp.name);
+
+            $('#modal-item-datails').show();
+        });
+    })
 });
