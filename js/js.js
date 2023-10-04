e0b37f91688de224f60c6bde6c34a0adefa171e5
Manuel Molina García <mmgmolina03@gmail.com>
2023-10-04 14:28:47 +0200

    En proceso de creacion y mejora del modal

--- js/js.js
+++ js/js.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
 
     $.ajax({
-        url: 'https://pokeapi.co/api/v2/item/?limit=300',
+        url: 'https://pokeapi.co/api/v2/item/?limit=700',
         type: 'GET'
     }).done(function (resp) {
         var listaItems = resp.results;
         listaItems.forEach(item => {
-            var template = `<div class="item" itemId="/*aqui tengo que averiguar como pasarle el id de los items para que al pulsar en cada uno salga el modal*/"> ${item.name} <div>`;
-            $('#lista-nomItems').append(template);
+            $.ajax({
+                url: item.url,
+                type: 'GET'
+            }).done(function(items){
+                var template= `
+                <div class="cardItem" itemId="${items.name}" style="width: 15%;">
+                <img class="card-img-top" src="${items.sprites.default}" alt="Foto de Item">
+                    <div class="card-body">
+                        <p class="card-text" style="text-align: center;">${items.name}</p>
+                    </div>
+                </div>`
+                $('#lista-nomItems').append(template);
+
+            });
+
         });
     });
-    $(document).on('click', '.item', function () {
-        var itemId = $(this).attr('itemid');
+    $(document).on('click', '.cardItem', function () {
+        var itemId = $(this).attr('itemId');
 
         $.ajax({
             url: 'https://pokeapi.co/api/v2/item/' + itemId,
