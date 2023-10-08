8c2c553e8fb142d5942b77f5992ca3f2e087c4e6
Manolo <mmgmolina03@gmail.com>
2023-10-08 18:50:55 +0200

    todo finalizado a esperas de la aprovacion en la pull request

--- js/js.js
+++ js/js.js
@@ -1,40 +1,101 @@
 $(document).ready(function () {
-    var numItems;
-    var pag;
-    var numItemPag=24;
+    var currentPage = 1;
+    var totalPages = 0;
+    var campoBuscar = $('#campoBuscar');
+
+    function generatePaginator(totalPages) {
+        $('#pagination').empty();
+        var startPage = Math.max(currentPage - 2, 1);
+        var endPage = Math.min(startPage + 4, totalPages);
+
+        if (startPage > 1) {
+            var prev = $('<li class="page-item"><a class="btn" data-page="' + (startPage - 1) + '"><</a></li>');
+            $('#pagination').append(prev);
+        }
+
+        for (var i = startPage; i <= endPage; i++) {
+            var template = $('<li class="page-item"><a class="btn" data-page="' + i + '">' + i + '</a></li>');
+            $('#pagination').append(template);
+        }
+
+        if (endPage < totalPages) {
+            var next = $('<li class="page-item"><a class="btn" data-page="' + (endPage + 1) + '">></a></li>');
+            $('#pagination').append(next);
+        }
+    }
+
     $.ajax({
-        url: 'https://pokeapi.co/api/v2/item/?limit=24',
-        type: 'GET'
+        type: "GET",
+        url: "https://pokeapi.co/api/v2/item?limit=24",
     }).done(function (resp) {
-        numItems = resp.count;
-        pag= Math.trunc(numItems/numItemPag)+1;
-        for (var i = 1; i < pag; i++) {
-            var template= ` <li class="page-item">${i} </li>`
-            $('#paginas').append(template);
-            debugger;
-        };
-        var listaItems = resp.results;
-
-        listaItems.forEach(item => {
-            $.ajax({
-                url: item.url,
-                type: 'GET'
-            }).done(function (items) {
-                var template = `
-                <div class="cardItem" itemId="${items.name}" style="width: 15%; text-align: center;">
-                <img class="card-img-top" src="${items.sprites.default}" alt="Foto de Item" style="width: 50%;">
-                    <div class="card-body">
-                        <p class="card-text">${items.name}</p>
-                    </div>
-                </div>`
-                $('#lista-nomItems').append(template);
-
-            });
-
+        generatePaginator(resp.count / 20);
+        loadItemList(currentPage);
+        $('#pagination').on('click', 'a.btn', function () {
+            var clickedPage = $(this).data('page');
+            if (clickedPage === 'prev' && currentPage > 1) {
+                currentPage = Math.max(currentPage - 1, 1);
+            } else if (clickedPage === 'next' && currentPage < totalPages) {
+                currentPage = Math.min(currentPage + 1, totalPages);
+            } else {
+                currentPage = clickedPage;
+            }
+            loadItemList(currentPage);
         });
     });
-    
-    $(document).on('click', '.cardItem', function () {  
+
+    function loadItemList(page) {
+        $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/item?limit=24&offset=${(page - 1) * 24}`,
+        }).done(function (resp) {
+            var listaItem = resp.results;
+            $('#lista-nomItems').empty();
+            listaItem.forEach(item => {
+                $.ajax({
+                    type: "GET",
+                    url: item.url
+                }).done(function (ItemResponse) {
+                    var template = `
+                    <div class="cardItem" id="cardItem" itemId="${ItemResponse.name}" style="width: 15%; text-align: center;">
+                        <img class="card-img-top" src="${ItemResponse.sprites.default}" alt="Foto de Item" style="width: 50%;">
+                        <div class="card-body">
+                            <p class="card-text">${ItemResponse.name}</p>
+                        </div>
+                    </div>`;
+                    $('#lista-nomItems').append(template);
+                });
+            });
+            generatePaginator(resp.count / 20);
+        });
+
+    }
+    $('#searchButton').on('click', function () {
+        var itemName = campoBuscar.val().toLowerCase();
+        filterItemByName(itemName);
+    });
+
+    $('#campoBuscar').on('keypress', function () {
+        var itemName = campoBuscar.val().toLowerCase();
+        filterItemByName(itemName);
+
+    });
+
+    function filterItemByName(name) {
+        var allItemCards = $('.cardItem');
+
+        allItemCards.each(function () {
+            var cardName = $(this).find('.card-text').text().toLowerCase();
+            if (cardName.includes(name)) {
+
+                $(this).show();
+            } else {
+
+                $(this).hide();
+            }
+        });
+    }
+
+    $(document).on('click', '.cardItem', function () {
         var itemId = $(this).attr('itemId');
         $.ajax({
             url: 'https://pokeapi.co/api/v2/item/' + itemId,
@@ -47,4 +108,5 @@ $(document).ready(function () {
             $('#modal-item-details').modal('show');
         });
     });
+
 });
